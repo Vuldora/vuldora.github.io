c50106586bf7001191cca1103c26b670c1d88f23
Vivi <dsafjaifajfifsa>
2026-05-24 21:55:06 +0300

    Update UZA, update all pages

--- script/gameFrame.js
+++ script/gameFrame.js
@@ -1,18 +1,31 @@
-const btn = document.getElementById("fsBtn");
-  const iframe = document.getElementById("gameFrame");
+let params = new URLSearchParams(document.location.search);
+let game = params.get("game");
 
-  btn.addEventListener("click", async () => {
-    if (!document.fullscreenElement) {
-      // enter fullscreen
-      if (iframe.requestFullscreen) {
-        await iframe.requestFullscreen();
-      } else if (iframe.webkitRequestFullscreen) { // Safari
-        iframe.webkitRequestFullscreen();
-      } else if (iframe.msRequestFullscreen) { // old IE/Edge
-        iframe.msRequestFullscreen();
+const btn = document.getElementById("fsBtn");
+const gameTitle = document.getElementById("gameTitle");
+const iframe = document.getElementById("gameFrame");
+
+switch (game) {
+	case "uza":
+		gameTitle.innerHTML = "Unnamed Zombie Assasult";
+		iframe.src = "/games/uza.html";
+		break;
+	case "chicken-collector":
+		gameTitle.innerHTML = "Chicken Collector";
+		iframe.src = "/games/chicken-collector.html";
+		break;
+}
+
+btn.addEventListener("click", async () => {
+	if (!document.fullscreenElement) {
+		if (iframe.requestFullscreen) {
+			await iframe.requestFullscreen();
+      } else if (iframe.webkitRequestFullscreen) {
+			iframe.webkitRequestFullscreen();
+      } else if (iframe.msRequestFullscreen) {
+			iframe.msRequestFullscreen();
       }
     } else {
-      // exit fullscreen
       if (document.exitFullscreen) {
         await document.exitFullscreen();
       } else if (document.webkitExitFullscreen) {
